88826555450d206b413f9014453402289c4380dd
psguazz <p.sguazz@gmail.com>
2024-05-30 00:23:01 +0200

    Limiting episodes

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ if __name__ == '__main__':
     s_t = env.reset()
 
     agent = Agent(n_actions=env.action_space.n)
+    steps = 0
 
     for step in range(STEPS):
+        print(steps)
+        steps += 1
         a_t, p_t = agent.choose([s_t])[0]
         s_t1, r_t1, done, info = env.step(a_t)
 
@@ -23,8 +26,9 @@ if __name__ == '__main__':
             done=done
         )
 
-        if done:
+        if done or steps > 500:
             s_t = env.reset()
+            steps = 0
         else:
             s_t = s_t1
 
