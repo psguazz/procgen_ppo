d20a85b25f96ebf95d83d4127c0c93075fa67444
psguazz <p.sguazz@gmail.com>
2024-06-04 00:52:18 +0200

    Conv net

--- ppo/actor_critic.py
+++ ppo/actor_critic.py
@@ -1,22 +1,35 @@
 import tensorflow as tf
 import tensorflow.keras as keras
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
 
 
 class ActorCritic(keras.Model):
     def __init__(self, num_actions):
         super().__init__()
 
-        self.common = Dense(128, activation="relu")
+        self.common_layers = [
+            Conv2D(16, (3, 3)),
+            MaxPooling2D((2, 2)),
+            Conv2D(32, (3, 3)),
+            MaxPooling2D((2, 2)),
+            Conv2D(64, (3, 3)),
+            MaxPooling2D((2, 2)),
+            Flatten(),
+            Dense(64, activation="relu"),
+            Dense(num_actions)
+        ]
+
         self.actor = Dense(num_actions)
         self.critic = Dense(1)
 
-    def call(self, inputs):
-        x = self.common(inputs)
+    def call(self, x):
+        for layer in self.common_layers:
+            x = layer(x)
+
         return self.actor(x), self.critic(x)
 
     def choose(self, state):
-        inputs = tf.convert_to_tensor([state])
+        inputs = self._preprocess(state)
         logits, value = self.call(inputs)
 
         action = tf.random.categorical(logits, 1)[0, 0]
@@ -24,3 +37,6 @@ class ActorCritic(keras.Model):
         log_prob = tf.math.log(prob)
 
         return [tf.squeeze(x) for x in [action, value, log_prob]]
+
+    def _preprocess(self, state):
+        return tf.convert_to_tensor([state], dtype=tf.float32) / 255.0
