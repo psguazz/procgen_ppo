b33d1ce9e5441255b7d4773acd67560f32552d35
psguazz <p.sguazz@gmail.com>
2024-06-04 00:34:51 +0200

    Using procgen env

--- env.py
+++ env.py
@@ -5,8 +5,9 @@ import gym
 class Env:
     def __init__(self):
         self.env = gym.make(
-            "CartPole-v1",
+            "procgen:procgen-coinrun-v0",
             render_mode="human",
+            distribution_mode="easy"
         )
 
         self.n_actions = self.env.action_space.n
@@ -16,13 +17,13 @@ class Env:
     def step(self, action):
         action = action.numpy()
 
-        self.state, self.reward, term, trunc, _ = self.env.step(action)
-        self.done = term or trunc
+        self.state, self.reward, term, _ = self.env.step(action)
+        self.done = term
 
         return self.state, self.reward
 
     def reset(self):
-        self.state, _ = self.env.reset()
+        self.state = self.env.reset()
         self.done = False
 
         return self.state
