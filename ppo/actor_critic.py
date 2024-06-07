2aa4b0327bd454d9b3ef5a39c8cb3bbbca5f5559
psguazz <p.sguazz@gmail.com>
2024-06-07 14:37:04 +0200

    Addressing exploding gradient
    I think? I was getting a bunch of NaNs out of the network before this.

--- ppo/actor_critic.py
+++ ppo/actor_critic.py
@@ -1,22 +1,25 @@
 import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras.initializers import RandomNormal
 
 
 class ActorCritic(keras.Model):
     def __init__(self, num_actions):
         super().__init__()
 
+        init = RandomNormal(mean=0., stddev=0.1)
+
         self.common_layers = [
-            Conv2D(16, (3, 3)),
+            Conv2D(16, (3, 3), kernel_initializer=init),
             MaxPooling2D((2, 2)),
-            Conv2D(32, (3, 3)),
+            Conv2D(32, (3, 3), kernel_initializer=init),
             MaxPooling2D((2, 2)),
-            Conv2D(64, (3, 3)),
+            Conv2D(64, (3, 3), kernel_initializer=init),
             MaxPooling2D((2, 2)),
             Flatten(),
-            Dense(64, activation="relu"),
-            Dense(num_actions)
+            Dense(64, activation="relu", kernel_initializer=init),
+            Dense(num_actions, kernel_initializer=init)
         ]
 
         self.actor = Dense(num_actions)
@@ -29,7 +32,7 @@ class ActorCritic(keras.Model):
         return self.actor(x), self.critic(x)
 
     def choose(self, state):
-        inputs = self._preprocess(state)
+        inputs = self._preprocess([state])
         logits, value = self.call(inputs)
 
         action = tf.random.categorical(logits, 1)[0, 0]
@@ -38,5 +41,18 @@ class ActorCritic(keras.Model):
 
         return [tf.squeeze(x) for x in [action, value, log_prob]]
 
-    def _preprocess(self, state):
-        return tf.convert_to_tensor([state], dtype=tf.float32) / 255.0
+    def eval(self, states, actions):
+        inputs = self._preprocess(states)
+        logits, values = self.call(inputs)
+
+        indices = tf.range(logits.shape[0])
+        indices = tf.stack([indices, actions], axis=1)
+
+        probs = tf.nn.softmax(logits)
+        probs = tf.gather_nd(probs, indices)
+        log_probs = tf.math.log(probs)
+
+        return [tf.squeeze(x) for x in [values, log_probs]]
+
+    def _preprocess(self, states):
+        return tf.convert_to_tensor(states, dtype=tf.float32) / 255.0
--- ppo/config.py
+++ ppo/config.py
@@ -1,16 +1,13 @@
 import numpy as np
 
-ALPHA = 0.01
+ALPHA = 0.0003
 GAMMA = 0.99
 EPS = np.finfo(np.float32).eps.item()
+CLIP = 0.2
 
 EPOCHS = 3
 BATCHES = 4
 BATCH_SIZE = 128
 
-# ALPHA = 0.0003
-# GAMMA = 0.99
-# CLIP = 0.2
-#
 # ACTOR_PATH = "/Users/psg/master_ai/autonomous/project/ppo/weights/actor.weights.h5"
 # CRITIC_PATH = "/Users/psg/master_ai/autonomous/project/ppo/weights/critic.weights.h5"
