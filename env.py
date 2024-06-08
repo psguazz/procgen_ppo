cf86b82bfab5c320dfa605d5d21f828cb565ceb0
psguazz <p.sguazz@gmail.com>
2024-06-08 22:26:02 +0200

    Logging

--- env.py
+++ env.py
@@ -20,11 +20,20 @@ class Env:
 
         self.state, self.reward, term, _ = self.env.step(action)
         self.done = term
+        self.steps += 1
+
+        print(f"Step {self.steps} / Action {action} / Reward {self.reward}")
+
+        if self.done:
+            print("Done!")
 
         return self.state, self.reward
 
     def reset(self):
         self.state = self.env.reset()
         self.done = False
+        self.steps = 0
+
+        print("Resetting environment!")
 
         return self.state
